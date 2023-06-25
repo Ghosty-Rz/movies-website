b2139eee69b393483b1d8da442fc51b5343fb292
Rihab Zouitni <zouitnirihab@gmail.com>
2023-06-25 16:35:18 +0100

    banner almost complete

--- Components/Banner.tsx
+++ Components/Banner.tsx
@@ -1,10 +1,53 @@
-function Banner() {
-  return (
-    <div>
-        <div>
-            <Image/> 
+import Image from "next/image"
+import { Movie } from '../typings'
+import { useEffect, useState } from "react"
+import { baseUrl } from "@/Constants/movie"
+import {FaPlay} from "react-icons/fa"
+import { InformationCircleIcon } from "@heroicons/react/solid"
+
+interface Props {
+    netflixOriginals: Movie[]
+}
+{/** this "?" makes it optional */}
+
+
+
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
+
+  console.log(movie)
+
+    return (
+        <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:pb-12 pl-2 lg:justify-end">
+        
+          <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+            <Image
+              src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+              alt=""
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        
+        <div className="pt-40">
+        <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+            {movie?.title || movie?.name || movie?.original_name}</h1>
+        <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+            {movie?.overview}</p>
         </div>
-    </div>
+            <div className="flex space-x-3">
+                <button className="BannerButton bg-white text-black">
+                    <FaPlay className="h-4 w-4 text-black md:h-7"/>
+                    Play</button>
+                <button className="BannerButton bg-[gray]/70">More Info<InformationCircleIcon className="text-black h-5 w-5 md:h-8 md:w-8"/></button>
+            </div>
+      </div>
   )
 }
 
